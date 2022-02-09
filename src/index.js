42857e4d53632207bc610d6f50c269232f5c87b5
Safia <safia.ibrahim2121@gmail.com>
2022-02-09 17:49:44 +0000

    added list names

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 console.log(data);
 
-// WRITE YOUR CODE BELOW!
+// create a functions that contains all the list items
+function buttons() {
+  // LOOP through the data
+  data.filter(function (dog) {
+    const ulElement = document.querySelector("ul");
+    const liElement = document.createElement("li");
+    ulElement.append(liElement);
+    liElement.innerText = dog.name;
+    liElement.className = "dogs-list__button";
+  });
+}
+buttons();
